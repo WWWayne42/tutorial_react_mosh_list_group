b06c46bd5b514c450f049d8898eabeef3bf03b78
Your Name <your.email@example.com>
2025-11-17 16:42:41 +1100

    Passing Data (Items and heading) from App (Parent Component) to ListGroup (Child Component) via props (父组件通过JSX属性传递)

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
+// 目标：动态调整列表【标题】，【内容】
+// {items:[], heading:string}
+// TypeScript功能： 接口interface（形状shape），用来定义一个object的形状、接口
 
-function ListGroup() {
-  let items = ["New York", "Tokyo", "London", "Paris"];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
+// 如下使用了参数解构destructuring。如果不使用结构：function ListGroup(props: ListGroupProps)
+// TypeScript 检查类型，语法是用冒号+interface，比如 “: ListGroupProps”
+function ListGroup({ items, heading }: ListGroupProps) {
   //hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   // arr[0]  variable: current state value(selectedIndex)
@@ -10,7 +18,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>There are no items in the list.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
