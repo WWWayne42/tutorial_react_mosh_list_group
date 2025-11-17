a321b351b64a4703bee96d47bca26b33c379da2d
Your Name <your.email@example.com>
2025-11-17 19:53:01 +1100

    Add function callback props pattern explanation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "Tokyo", "London", "Paris"];
-  
+
+  //不同叫法：回调函数; 事件处理函数（Event Handler）;事件处理回调（Event Handler Callback）
+  // 父组件定义回调函数（业务逻辑）
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  //父组件通过 props 传递回调函数：当前父组件中的事件处理回调函数，通过 onSelectItem 属性，注入（传递）给子组件
+  // 父组件提供 handle（handle前缀清楚地表示"这是父组件的处理逻辑"）
+  // 子组件暴露 on 事件点（on前缀表示"这是子组件的事件接口"）
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
-// 目标：动态调整列表【标题】，【内容】
-// {items:[], heading:string}
-// TypeScript功能： 接口interface（形状shape），用来定义一个object的形状、接口
-
 interface ListGroupProps {
   items: string[];
   heading: string;
+  // (item: string): void;  // 函数类型的props
+  onSelectItem: (item: string) => void; // 这是一个事件点（暴露给使用者的接口），类似于onClick， 
+  // ↑ 这个名字从子组件角度表达："这是一个当选择项目时要调用的回调"
 }
 
 // 如下使用了参数解构destructuring。如果不使用结构：function ListGroup(props: ListGroupProps)
 // TypeScript 检查类型，语法是用冒号+interface，比如 “: ListGroupProps”
-function ListGroup({ items, heading }: ListGroupProps) {
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   //hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   // arr[0]  variable: current state value(selectedIndex)
@@ -30,6 +29,7 @@ function ListGroup({ items, heading }: ListGroupProps) {
             }
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
             key={item}
           >
