eed5a08d7b6a0ddd6efa2d43b7fe27ae1d6d125b
Your Name <your.email@example.com>
2025-11-18 17:12:00 +1100

    Children Prop 特殊属性(能够接收复杂数据传入,使用的数据规范TS: ReactNode): Passing Mixed Data (HTML, JSX, etc.) from App (Parent Component) to ListGroup (Child Component) via props (父组件通过JSX属性传递)

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,7 +1,12 @@
-const Alert = () => {
-  return (
-    <div>Alert</div>
-  )
+import { type ReactNode } from "react";
+
+// children 是特殊的 prop，所有的组件都可以接收
+interface AlertProps {
+  children: ReactNode; // children 是特殊的 prop
 }
 
-export default Alert
+const Alert = ({ children }: AlertProps) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+
+export default Alert;
